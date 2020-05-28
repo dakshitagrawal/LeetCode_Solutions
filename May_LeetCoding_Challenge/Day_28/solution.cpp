2ed5e12f914bc4cda0284ab820deeb89062bf9fe
dakshitagrawal97 <neelagrawal08@gmail.com>
2020-05-28 19:33:31 +0530

    :books: Improve explanation of odd-even solution for Day 28 problem

--- May_LeetCoding_Challenge/Day_28/solution.cpp
+++ May_LeetCoding_Challenge/Day_28/solution.cpp
@@ -41,7 +41,22 @@ public:
     bit to the previous number. Even numbers, 
     on the other hand, increase their `1` bit 
     count to the common part of their number 
-    and previous number.
+    and previous number. 
+    
+    Further Explanation:
+    When the next even number comes, all bits 
+    beyond the common part are changed to zero, 
+    and the bit `1` is added just after the 
+    common part.
+
+    e.g. - 11 is 1011
+           12 is 1100
+
+           Basically there is a flip of all bits 
+           beyond the common part between a 
+           number and its predecessor, or in
+           other terms, an addition of 1 to the 
+           count of bits of the common part.
 
     ---------------------------------------------------------------------------------------------------
 */
